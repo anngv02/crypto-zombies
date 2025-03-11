9f71af46fc65a63b59eab961ec239e929876109a
unknown <antpvinh666@gmail.com>
2025-03-11 10:54:26 +0700

    crypto-zombies second

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     bsctestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-      accounts: [`0x8b47c499d3db7f06ca82152785175c353094899d32b5fee8b39c36cf353ab1b6`],
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
     },
   },
 };
